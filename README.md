18c32535ac25c386ff978f3e170a2fbd17af96ac
jweob <jweob@cantab.net>
2016-07-20 21:18:02 -0400

    Added license and 3D files for mounts

--- README.md
+++ README.md
@@ -1,4 +1,24 @@
-# RepRap-Safety
-Safety cutoff for RepRap 3D printers
 
-USE AT YOUR OWN RISK, ABSOLUTELY NO WARRANTY EXPRESSED OR IMPLIED
+RepRap safety cutoff
+Copyright John O'Brien 2016
+jweob@cantab.net 2016-07-16
+
+This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
+Turns off the power supply to a RepRap using a relay if any of the following three conditions are met
+1. Secondary thermistor reads a temperature above a set threshold
+2. Bed heat signal is on for more than a set period of time
+3. Hot end is on for more than a set period of time
+
--- reprap-safety/reprap-safety.ino
+++ reprap-safety/reprap-safety.ino
@@ -1,11 +1,28 @@
 /*
 RepRap safety cutoff
+Copyright John O'Brien 2016
+jweob@cantab.net 2016-07-16
+
+This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+
 Turns off the power supply to a RepRap using a relay if any of the following three conditions are met
 1. Secondary thermistor reads a temperature above a set threshold
 2. Bed heat signal is on for more than a set period of time
 3. Hote end is on for more than a set period of time
 
-John O'Brien jweob@cantab.net 2016-0716
+
 */
 
 #include <avr/pgmspace.h>
@@ -62,7 +79,7 @@ static float analog2temp(unsigned long raw); // Converts the reading from the ov
 void updatePins(); // Updates the relay and LED indicator pins based on the current state of the printer
 
 // Thermistor table. This table is setup for the Extruder thermistor: Digikey 480-3137-ND which should be included in All Huxleys shipped after 25/2/14
-// Table was generated using the RepRapProFormula
+// Table was generated using the RepRapPro Formula
 const short temptable[][2] PROGMEM = {
 {23*OVERSAMPLENR,    341},
 {25*OVERSAMPLENR,    333},
@@ -143,6 +160,7 @@ void setup()
   pinMode(exPosPin, INPUT);
   pinMode(exNegPin, INPUT);
   updatePins();
+  delay( 100 ); // Give voltage on the A0 pin time to settle
 }
 
 
